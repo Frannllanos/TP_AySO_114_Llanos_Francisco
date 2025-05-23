68efe373d51055a25d61cab3a9d42bcf8a57ee76
Frannllanos <llanosfran044@gmail.com>
2025-05-23 14:10:37 +0000

    actualizado

--- script_dir.sh
+++ script_dir.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Crear estructura de directorios
-mkdir -p ~/materia1/uni1 ~/materia1/uni2 ~/materia1/uni3 \
-         ~/materia2/uni1 ~/materia2/uni2 ~/materia2/uni3
+mkdir -p ~/{materia1,materia2}/{uni1,uni2,uni3}
 
 # Mostrar estructura con tree desde el Home
 cd ~
